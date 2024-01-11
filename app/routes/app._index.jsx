b8a87368715bce773b163b296af9cfb3354fbc57
Muhammad Fayzan Siddiqui <muhammadfayzan.siddiqui@bertelsmann.de>
2024-01-12 04:40:45 +0800

    Initial commit

---
+++ app/routes/app._index.jsx
@@ -0,0 +1,146 @@
+import { useEffect } from "react";
+import { json } from "@remix-run/node";
+import { useActionData, useNavigation, useSubmit } from "@remix-run/react";
+import {
+  Page,
+  Layout,
+  Text,
+  Card,
+  Button,
+  BlockStack,
+  Box,
+  List,
+  Link,
+  InlineStack,
+} from "@shopify/polaris";
+import { authenticate } from "../shopify.server";
+
+export const loader = async ({ request }) => {
+  await authenticate.admin(request);
+
+  return null;
+};
+
+export const action = async ({ request }) => {
+  const { admin } = await authenticate.admin(request);
+  const color = ["Red", "Orange", "Yellow", "Green"][
+    Math.floor(Math.random() * 4)
+  ];
+  const response = await admin.graphql(
+    `#graphql
+      mutation populateProduct($input: ProductInput!) {
+        productCreate(input: $input) {
+          product {
+            id
+            title
+            handle
+            status
+            variants(first: 10) {
+              edges {
+                node {
+                  id
+                  price
+                  barcode
+                  createdAt
+                }
+              }
+            }
+          }
+        }
+      }`,
+    {
+      variables: {
+        input: {
+          title: `${color} Snowboard`,
+          variants: [{ price: Math.random() * 100 }],
+        },
+      },
+    }
+  );
+  const responseJson = await response.json();
+
+  return json({
+    product: responseJson.data?.productCreate?.product,
+  });
+};
+
+export default function Index() {
+  const nav = useNavigation();
+  const actionData = useActionData();
+  const submit = useSubmit();
+  const isLoading =
+    ["loading", "submitting"].includes(nav.state) && nav.formMethod === "POST";
+  const productId = actionData?.product?.id.replace(
+    "gid://shopify/Product/",
+    ""
+  );
+
+  useEffect(() => {
+    if (productId) {
+      shopify.toast.show("Product created");
+    }
+  }, [productId]);
+  const generateProduct = () => submit({}, { replace: true, method: "POST" });
+
+  function syncProduct() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json));
+  }
+
+  return (
+    <Page>
+      <ui-title-bar title="Stuller Integration">
+        <button variant="primary" onClick={syncProduct}>
+          Sync Products
+        </button>
+      </ui-title-bar>
+      <BlockStack gap="500">
+        <Layout>
+          <Layout.Section>
+            <Card>
+              <BlockStack gap="500">
+                <BlockStack gap="200">
+                  <Text as="h2" variant="headingMd">
+                    Welcome to Stuller Integration App
+                  </Text>
+                  <Text variant="bodyMd" as="p">
+                    This app load/sync products from Stuller API into Shopify store.
+                  </Text>
+                </BlockStack> 
+                <InlineStack gap="300">
+                  <Button loading={isLoading} onClick={syncProduct}>
+                    Sync Products
+                  </Button>
+                  {actionData?.product && (
+                    <Button
+                      url={`shopify:admin/products/${productId}`}
+                      target="_blank"
+                      variant="plain"
+                    >
+                      View product
+                    </Button>
+                  )}
+                </InlineStack>
+                {actionData?.product && (
+                  <Box
+                    padding="400"
+                    background="bg-surface-active"
+                    borderWidth="025"
+                    borderRadius="200"
+                    borderColor="border"
+                    overflowX="scroll"
+                  >
+                    <pre style={{ margin: 0 }}>
+                      <code>{JSON.stringify(actionData.product, null, 2)}</code>
+                    </pre>
+                  </Box>
+                )}
+              </BlockStack>
+            </Card>
+          </Layout.Section>
+        </Layout>
+      </BlockStack>
+    </Page>
+  );
+}
